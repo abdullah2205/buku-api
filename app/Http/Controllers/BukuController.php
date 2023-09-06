5ac0a2134b0ee42b0ad9860fc8bff382c7020f54
Muhammad Abdullah <ma22052000@gmail.com>
2023-09-06 16:14:28 +0700

    tambah fungsi untuk store buku

--- app/Http/Controllers/BukuController.php
+++ app/Http/Controllers/BukuController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Buku;
 
 class BukuController extends Controller
 {
@@ -15,5 +17,36 @@ class BukuController extends Controller
             'pesan' => 'List Buku',
             'data' => $bukus
         ]);
+    }
+    
+    public function store(Request $request)
+    {
+        // Validasi data yang diterima dari request
+        $validator = Validator::make($request->all(), [
+            'judul' => 'required|string',
+            'tahun' => 'required|string',
+        ]);
+
+        // Jika validasi gagal, kirim pesan kesalahan
+        if ($validator->fails()) {
+            return response()->json([
+                'pesan' => 'Validasi gagal', 
+                'errors' => $validator->errors()
+            ], 400);
+        }
+
+        // Buat buku baru
+        $user = $request->user();
+        $buku = Buku::create([
+            'judul' => $request->input('judul'),
+            'tahun' => $request->input('tahun'),
+            'user_id' => $user->id, // Menyimpan ID user pemilik buku
+        ]);
+
+        // Kirim respons dengan buku yang baru dibuat
+        return response()->json([
+            'pesan' => 'Buku berhasil dibuat', 
+            'data' => $buku
+        ], 201);
     }
 }
